591138d48bc9199af1ef15980032b8ed95f9bc12
MichaelRdot <mruss62@lsu.edu>
2025-11-24 18:09:55 -0600

    push again :)

---
+++ MTGProxyApp/Dtos/CardDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MTGProxyApp.Dtos;
+
+public class CardDto
+{
+    [JsonPropertyName("set_id")] public required string SetId { get; set; }
+    [JsonPropertyName("prints_search_uri")] public required Uri PrintsSearchUri { get; set; }
+    [JsonPropertyName("collector_number")] public string? CollectorNumber { get; set; }
+    [JsonPropertyName("highres_image")] public bool HighresImage { get; set; }
+    [JsonPropertyName("image_uris")] public CardPngDto? ImageUris { get; set; }
+    
+    public class CardPngDto
+    {
+        [JsonPropertyName("png")] public Uri? Png { get; set; }
+    }
+}
+
