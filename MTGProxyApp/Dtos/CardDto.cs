d28d7ac7db44b3efb9ed6259ea95ca80475ce39f
MichaelRdot <mruss62@lsu.edu>
2025-11-30 02:29:02 -0600

    Pushing what currently almost works. CardArtPicker is giving me some issues again, but most of it seems to be working fine.

--- MTGProxyApp/Dtos/CardDto.cs
+++ MTGProxyApp/Dtos/CardDto.cs
@@ -4,18 +4,22 @@ namespace MTGProxyApp.Dtos;
 
 public class CardDto
 {
+    public int Count { get; set; }
+    public int LineIndex { get; set; }
     [JsonPropertyName("set")] public required string Set { get; set; }
-
-    [JsonPropertyName("prints_search_uri")]
-    public required Uri PrintsSearchUri { get; set; }
-
+    [JsonPropertyName("prints_search_uri")] public required Uri PrintsSearchUri { get; set; }
     [JsonPropertyName("collector_number")] public string? CollectorNumber { get; set; }
     [JsonPropertyName("highres_image")] public bool HighresImage { get; set; }
     [JsonPropertyName("image_uris")] public CardPngDto? ImageUris { get; set; }
     [JsonPropertyName("name")] public string Name { get; set; }
-
+    [JsonPropertyName("card_faces")] public CardFaceDto[]? CardFaces { get; set; }
     public class CardPngDto
     {
         [JsonPropertyName("png")] public Uri? Png { get; set; }
+    }
+
+    public class CardFaceDto
+    {
+        [JsonPropertyName("image_uris")] public CardPngDto? ImageUris { get; set; }
     }
 }
