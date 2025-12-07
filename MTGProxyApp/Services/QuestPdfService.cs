9b2c3a9ef828d79d0f9e0d9599f4da7bfd8199d7
MichaelRdot <mruss62@lsu.edu>
2025-12-07 01:26:19 -0600

    Pull and fix asap

--- MTGProxyApp/Services/QuestPdfService.cs
+++ MTGProxyApp/Services/QuestPdfService.cs
@@ -8,73 +8,87 @@ namespace MTGProxyApp.Services;
 
 public class QuestPdfService
 {
-    private List<byte[]> _cardsPrints = new();
-    private bool _blackCorners = true;
-    private bool _borders = true;
-    private int _cardsDone;
-    
     private readonly float _cardHeight = UnitExtensions.ToPoints(88f, Millimetre);
     private readonly float _cardWidth = UnitExtensions.ToPoints(63f, Millimetre);
+    private readonly float _paddingTop = (PageSizes.Letter.Height - UnitExtensions.ToPoints(88f, Millimetre) * 3) / 2;
+
     private const float CrossLength = 8f;
     private const float CrossThickness = 1f;
 
-    public Task<byte[]> CreatePdf(List<byte[]> cardsPrints, bool blackCorners, bool borders)
+    public Task<byte[]> CreatePdf(List<List<byte[]>> cardsPrints, bool blackCorners, bool borders, bool printFlipCardsSeparate)
     {
-        _cardsPrints = cardsPrints;
-        _blackCorners = blackCorners;
-        _borders = borders;
-        _cardsDone = 0;
         var doc = Document.Create(doc =>
         {
-            var paddingTop = (PageSizes.Letter.Height - _cardHeight * 3) / 2;
-            while (_cardsDone != _cardsPrints.Count)
-                doc.Page(page =>
+            var cardPages = cardsPrints[0].Chunk(9).Select(chunk => chunk.ToList()).ToList();
+            foreach (var page in cardPages)
+            {
+                doc.Page(MakePage(page, blackCorners, borders));
+            }
+            if (printFlipCardsSeparate && cardPages[1].Count > 0)
+            {
+                cardPages = cardsPrints[1].Chunk(9).Select(chunk => chunk.ToList()).ToList();
+                foreach (var page in cardPages)
                 {
-                    page.Size(PageSizes.Letter);
-                    page.Margin(0);
-                    page.Background().AlignCenter().AlignMiddle().CutLine(1, PageSizes.Letter.Width,
-                        PageSizes.Letter.Height, _cardWidth, _cardHeight);
-
-                    page.Content().AlignCenter().PaddingTop(paddingTop).Table(table =>
-                    {
-                        table.ColumnsDefinition(columns =>
-                        {
-                            columns.ConstantColumn(_cardWidth);
-                            columns.ConstantColumn(_cardWidth);
-                            columns.ConstantColumn(_cardWidth);
-                        });
-                        for (var i = 0; i < 9 && _cardsDone < _cardsPrints.Count; i++)
-                        {
-                            table.Cell().Element(card =>
-                            {
-                                card
-                                    .Height(_cardHeight)
-                                    .Width(_cardWidth)
-                                    .Layers(layers =>
-                                    {
-                                        layers.PrimaryLayer()
-                                            .Border(_borders ? 1 : 0, Colors.Black)
-                                            .Background(_blackCorners ? Colors.Black : Colors.White)
-                                            .Image(_cardsPrints[_cardsDone])
-                                            .WithCompressionQuality(ImageCompressionQuality.Best)
-                                            .WithRasterDpi(300);
-                                        if (!_blackCorners) return;
-                                        layers.Layer().Element(e =>
-                                            e.AlignLeft().AlignTop().CornerCross(CrossLength, CrossThickness));
-                                        layers.Layer().Element(e =>
-                                            e.AlignRight().AlignTop().CornerCross(CrossLength, CrossThickness));
-                                        layers.Layer().Element(e =>
-                                            e.AlignLeft().AlignBottom().CornerCross(CrossLength, CrossThickness));
-                                        layers.Layer().Element(e =>
-                                            e.AlignRight().AlignBottom().CornerCross(CrossLength, CrossThickness));
-                                    });
-                            });
-                            _cardsDone++;
-                        }
-                    });
-                });
+                    doc.Page(MakePage(page, blackCorners, borders));
+                }
+                cardPages = cardsPrints[2].Chunk(9).Select(chunk => chunk.ToList()).ToList();
+                foreach (var page in cardPages)
+                {
+                    doc.Page(MakePage(page, blackCorners, borders));
+                }
+            }
         });
         return Task.FromResult(doc.GeneratePdf());
+    }
+
+    private Action<PageDescriptor> MakePage(List<byte[]> cards, bool blackCorners, bool borders)
+    {  
+        var cardsDone = 0;
+        return page =>
+        {
+            page.Size(PageSizes.Letter);
+            page.Margin(0);
+            page.Background().AlignCenter().AlignMiddle().CutLine(1, PageSizes.Letter.Width,
+                PageSizes.Letter.Height, _cardWidth, _cardHeight);
+
+            page.Content().AlignCenter().PaddingTop(_paddingTop).Table(table =>
+            {
+                table.ColumnsDefinition(columns =>
+                {
+                    columns.ConstantColumn(_cardWidth);
+                    columns.ConstantColumn(_cardWidth);
+                    columns.ConstantColumn(_cardWidth);
+                });
+                for (var i = 0; i < 9 && cardsDone < cards.Count; i++)
+                {
+                    table.Cell().Element(card =>
+                    {
+                        card
+                            .Height(_cardHeight)
+                            .Width(_cardWidth)
+                            .Layers(layers =>
+                            {
+                                layers.PrimaryLayer()
+                                    .Border(borders ? 1 : 0, Colors.Black)
+                                    .Background(blackCorners ? Colors.Black : Colors.White)
+                                    .Image(cards[cardsDone])
+                                    .WithCompressionQuality(ImageCompressionQuality.Best)
+                                    .WithRasterDpi(300);
+                                if (!blackCorners) return;
+                                layers.Layer().Element(e =>
+                                    e.AlignLeft().AlignTop().CornerCross(CrossLength, CrossThickness));
+                                layers.Layer().Element(e =>
+                                    e.AlignRight().AlignTop().CornerCross(CrossLength, CrossThickness));
+                                layers.Layer().Element(e =>
+                                    e.AlignLeft().AlignBottom().CornerCross(CrossLength, CrossThickness));
+                                layers.Layer().Element(e =>
+                                    e.AlignRight().AlignBottom().CornerCross(CrossLength, CrossThickness));
+                            });
+                    });
+                    cardsDone++;
+                }
+            });
+        };
     }
 
     private static class UnitExtensions
